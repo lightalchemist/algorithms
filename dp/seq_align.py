d185ab0c279fc2c278e322fc61bcb8d1782f335c
hongwei <hongwei.ng@adsc.com.sg>
2014-05-20 15:44:22 +0800

    Refactored code for sequence alignment.

--- dp/seq_align.py
+++ dp/seq_align.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""
+File: seq_align.py
+Author: Hong-Wei Ng
+Email: lightalchemist@gmail.com
+Github: https://github.com/lightalchemist
+Description: Performs sequence alignment of 2 strings.
+Code is based on pseudocode given in the book
+Algorithm Design by Jon Kleinberg and Eva Tardos
+"""
+
 from collections import defaultdict
 import numpy as np
 
@@ -15,103 +25,83 @@ def sequence_alignment_table(X, Y, gap_cost, mismatch_cost):
 
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if X[i - 1] == Y[j - 1]:
+            if X[i-1] == Y[j-1]:
                 m_cost = 0
             else:
-                m_cost = mismatch_cost[(X[i - 1], Y[j - 1])]
+                m_cost = mismatch_cost[(X[i-1], Y[j-1])]
 
-            A[i][j] = min(m_cost + A[i - 1][j - 1],
-                          gap_cost + A[i][j - 1],
-                          gap_cost + A[i - 1][j])
+            A[i][j] = min(m_cost + A[i-1][j-1],
+                          gap_cost + A[i][j-1],
+                          gap_cost + A[i-1][j])
 
     return A
 
 
-gap_symb = '_'
-def assemble2(A, X, Y, i, j):
-    if i < 0 and j < 0:
-        return "", ""
-
-    if i < 0 and j >= 0:
-        # return gap_symb * (j + 1), Y[:j+1]
-        return gap_symb * (j + 1), Y[:j+1]
-        # return gap_symb * (j), Y[:j]
-    if j < 0 and i >= 0:
-        # return X[:i+1], gap_symb * (i + 1)
-        return X[:i+1], gap_symb * (i + 1)
-        # return X[:i], gap_symb * (i)
-
-    if X[i-1] == Y[j-1]:  # Same. No cost.
-        Xi, Yi = assemble(A, X, Y, i-1, j-1)
-        return Xi + X[i-1], Yi + Y[j-1]
-
-    if A[i-1][j-1] < A[i][j-1] and A[i-1][j-1] < A[i-1][j]:  # mismatch
-        Xi, Yi = assemble(A, X, Y, i-1, j-1)
-        return Xi + X[i-1], Yi + Y[j-1]
-
-    if A[i][j-1] < A[i-1][j]:
-        Xi, Yi = assemble(A, X, Y, i, j-1)
-        return Xi + gap_symb, Yi + Y[j-1]
-    else:
-        Xi, Yi = assemble(A, X, Y, i-1, j)
-        return Xi + X[i-1], Yi + gap_symb
-
-
-import pdb
-def assemble(A, X, Y, u, v):
+def assemble(A, X, Y):
+    GAP_SYMBOL = '_'
     i, j = len(X), len(Y)
-    x = ""
-    y = ""
+    aligned_X, aligned_Y = "", ""
     while i > 0 and j > 0:
-
+        # Eact match or mismatch
         if A[i-1][j-1] < A[i-1][j] and A[i-1][j-1] < A[i][j-1]:
-            x = X[i-1] + x
-            y = Y[j-1] + y
+            aligned_X = X[i-1] + aligned_X
+            aligned_Y = Y[j-1] + aligned_Y
             i -= 1
             j -= 1
-
+        # Cheaper to leave a gap for X
         elif A[i][j-1] < A[i-1][j]:
-            x = gap_symb + x
-            y = Y[j-1] + y
+            aligned_X = GAP_SYMBOL + aligned_X
+            aligned_Y = Y[j-1] + aligned_Y
             j -= 1
-
+        # Cheaper to leave a gap for Y
         else:
-            x = X[i-1] + x
-            y = gap_symb + y
+            aligned_X = X[i-1] + aligned_X
+            aligned_Y = GAP_SYMBOL + aligned_Y
             i -= 1
 
-
-    # pdb.set_trace()
+    # Add gaps to pad remaining chars from X
     if i > 0:
-        y = gap_symb * (i) + y
-        x = X[:i] + x
+        aligned_Y = GAP_SYMBOL*i + aligned_Y
+        aligned_X = X[:i] + aligned_X
 
+    # Add gaps to pad remaining chars from Y
     if j > 0:
-        x = gap_symb * (j) + x
-        y = Y[:j] + y
+        aligned_X = GAP_SYMBOL*j + aligned_X
+        aligned_Y = Y[:j] + aligned_Y
 
-    return x, y
+    return aligned_X, aligned_Y
 
 
 def align(X, Y, gap_cost, mismatch_cost):
     A = sequence_alignment_table(X, Y, gap_cost, mismatch_cost)
-    print(A)
-    sx, sy = assemble(A, X, Y, len(X), len(Y))
+    aligned_X, aligned_Y = assemble(A, X, Y)
+    alignment_cost = A[len(X), len(Y)]
+
+    print('-' * max(len(X), len(Y)))
+    print("Given strings: ")
     print (X)
     print(Y)
     print('-' * max(len(X), len(Y)))
-    print(sx)
-    print(sy)
+    print("Aligned strings: ")
+    print(aligned_X)
+    print(aligned_Y)
+    print("Cost: {}".format(alignment_cost))
 
 
 def test():
     gap_cost = 1
+    # Dictionary of mismatch cost. Default cost currently set to 2.
+    # Consider implementing as an array instead for faster access.
     MISMATCH_COST = defaultdict(lambda: 2)
 
     X, Y = "stop", "tops"
-    X, Y = "bus stop", "top"
+    X, Y = "bus stops here", "top"
     X, Y = "atop", "top"
     X, Y = "atop", "top-class"
+    X, Y = "jennifer aniston", "angelina jolie"
+    X, Y = "tom hanks is sam", "tom cruise robs bank"
+    X, Y = "gold digger", "rube goldberg"
+    X, Y = "will smith in mib", "smithsonian museums"
 
     align(X, Y, gap_cost, MISMATCH_COST)
 
