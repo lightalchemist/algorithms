a4d2801463810e0129448010e5581af514c48422
hongwei <hongwei.ng@adsc.com.sg>
2014-05-20 10:59:18 +0800

    Implemented backtracking to compute a longest common subsequence of 2 strings.

--- lcs.py
+++ lcs.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
 
-def print_matrix(A):
-    m = len(A)
-    n = len(A[0])
-    for i in range(m):
-        # for j in range(n):
-        print " ".join(map(str, A[i]))
+"""
+File: lcs.py
+Author: Hong-Wei Ng
+Email: lightalchemist@gmail.com
+Github: https://github.com/lightalchemist
+Description: Implements the longest common subsequence algorithm using DP.
+Implementation follows pseudocode given in the book: Algorithms Unlocked by Thomas Cormen.
+"""
 
 import numpy as np
-def lcs(X, Y):
+
+
+def lcs_table(X, Y):
     """Computes the least common subsequence of string X and Y."""
     m, n = len(X), len(Y)
-    A = [[0] * (n+1) for _ in range(m+1)]
 
     A = np.zeros((m+1, n+1), dtype=np.uint)
     for i in range(1, m+1):
@@ -20,16 +24,49 @@ def lcs(X, Y):
             else:
                 A[i][j] = max(A[i-1][j], A[i][j-1])
 
-    # print_matrix(A)
-    print(A)
-    return A[m][n]
+    return A
+
+
+def assemble_lcs(A, X, Y, i, j):
+    if A[i][j] == 0:
+        return ""
+    elif X[i-1] == Y[j-1]:
+        return assemble_lcs(A, X, Y, i-1, j-1) + X[i-1]
+    elif A[i][j-1] > A[i-1][j]:
+        return assemble_lcs(A, X, Y, i, j-1)
+    else:
+        return assemble_lcs(A, X, Y, i-1, j)
+
+
+def longest_common_sequence(X, Y):
+    A = lcs_table(X, Y)
+    return assemble_lcs(A, X, Y, len(X), len(Y))
 
 
 def test():
     X = "XMJYAUZ"
     Y = "MZJAWXU"
-    # assert lcs(X, Y) == 4
-    print(lcs(X, Y))
+    assert longest_common_sequence(X, Y) == "MJAU"
+
+    X = "ABCDGH"
+    Y = "AEDFHR"
+    assert longest_common_sequence(X, Y) == "ADH"
+
+    X = "AGGTAB"
+    Y = "GXTXAYB"
+    assert longest_common_sequence(X, Y) == "GTAB"
+
+    X = ""
+    Y = ""
+    assert longest_common_sequence(X, Y) == ""
+
+    X = "A"
+    Y = "B"
+    assert longest_common_sequence(X, Y) == ""
+
+    X = ""
+    Y = "B"
+    assert longest_common_sequence(X, Y) == ""
 
 
 if __name__ == "__main__":
