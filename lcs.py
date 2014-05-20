019944f291441892fb5e466200e8d43efc2a7c9f
hongwei <hongwei.ng@adsc.com.sg>
2014-05-20 11:04:07 +0800

    Fixed comment for lcs_table(). Refactored code. Added docstrings.

--- lcs.py
+++ lcs.py
@@ -13,32 +13,39 @@ import numpy as np
 
 
 def lcs_table(X, Y):
-    """Computes the least common subsequence of string X and Y."""
+    """Computes the table, A[i][j],  needed to reconstruct
+    longest common subsequence of string X and Y.
+    A[i][j] stores length of longest subsequence between string
+    prefixes X[:i] and Y[:j].
+    """
     m, n = len(X), len(Y)
 
-    A = np.zeros((m+1, n+1), dtype=np.uint)
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                A[i][j] = 1 + A[i-1][j-1]
+    A = np.zeros((m + 1, n + 1), dtype=np.uint)
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                A[i][j] = 1 + A[i - 1][j - 1]
             else:
-                A[i][j] = max(A[i-1][j], A[i][j-1])
+                A[i][j] = max(A[i - 1][j], A[i][j - 1])
 
     return A
 
 
 def assemble_lcs(A, X, Y, i, j):
+    """Reconstruct the longest common subsequence of strings X and Y
+    by backtracking through the lcs table."""
     if A[i][j] == 0:
         return ""
-    elif X[i-1] == Y[j-1]:
-        return assemble_lcs(A, X, Y, i-1, j-1) + X[i-1]
-    elif A[i][j-1] > A[i-1][j]:
-        return assemble_lcs(A, X, Y, i, j-1)
+    elif X[i - 1] == Y[j - 1]:
+        return assemble_lcs(A, X, Y, i - 1, j - 1) + X[i - 1]
+    elif A[i][j - 1] > A[i - 1][j]:
+        return assemble_lcs(A, X, Y, i, j - 1)
     else:
-        return assemble_lcs(A, X, Y, i-1, j)
+        return assemble_lcs(A, X, Y, i - 1, j)
 
 
 def longest_common_sequence(X, Y):
+    """Returns the longest common subsequence of strings X and Y."""
     A = lcs_table(X, Y)
     return assemble_lcs(A, X, Y, len(X), len(Y))
 
