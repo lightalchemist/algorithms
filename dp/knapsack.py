4cb60b8e9b63f5d744c2cabbe5e2bfda7ca6e6ba
hongwei <hongwei.ng@adsc.com.sg>
2014-05-21 17:29:24 +0800

    Added test cases.

--- dp/knapsack.py
+++ dp/knapsack.py
@@ -8,6 +8,9 @@ Github: https://github.com/lightalchemist
 Description: DP solution to knapsack problem.
 Solution closely follows pseudocode given in the book
 Algorithm Design by Jon Kleinberg and Eva Tardos.
+
+Test cases are taken from
+http://people.sc.fsu.edu/~jburkardt/datasets/knapsack_01/knapsack_01.html
 """
 
 
@@ -47,19 +50,60 @@ def assemble_solution(opt, values, weights, capacity):
     return selected
 
 
+def knapsack(values, weights, capacity):
+    opt = compute_opt_table(values, weights, capacity)
+    selected = assemble_solution(opt, values, weights, capacity)
+    return selected
+
+
 def test():
     capacity = 165
     weights = [23,31,29,44,53,38,63,85,89,82]
     values = [92,57,49,68,60,43,67,84,87,72]
-    selected = [1,1,1,1,0,1,0,0,0,0]
-    selected = [i for i in range(len(selected)) if selected[i] == 1]
-    print(selected)
+    solution = [1,1,1,1,0,1,0,0,0,0]
+    solution = [i for i in range(len(solution)) if solution[i] == 1]
+    solution = sorted(solution)
 
-    opt = compute_opt_table(values, weights, capacity)
-    print("Optimal value: {}".format(opt[len(values)][capacity]))
+    selected = knapsack(values, weights, capacity)
+    selected = sorted(selected)
+    assert selected == solution
 
-    selected = assemble_solution(opt, values, weights, capacity)
-    print(selected)
+    capacity = 26
+    weights = [12,7,11,8,9]
+    values = [24,13,23,15,16]
+    solution = [0,1,1,1,0]
+    solution = [i for i in range(len(solution)) if solution[i] == 1]
+    solution = sorted(solution)
+
+    selected = knapsack(values, weights, capacity)
+    selected = sorted(selected)
+    assert selected == solution
+
+    capacity = 190
+    weights = [56,59,80,64,75,17]
+    values = [50,50,64,46,50,5]
+    solution = [1,1,0,0,1,0]
+    solution = [i for i in range(len(solution)) if solution[i] == 1]
+    solution = sorted(solution)
+
+    selected = knapsack(values, weights, capacity)
+    selected = sorted(selected)
+    assert selected == solution
+
+    # capacity = 6404180
+    # weights = [382745,799601,909247,729069,467902,44328,34610,698150,823460,
+    #            903959,853665,551830,610856,670702,488960,951111,323046,446298,
+    #            931161, 31385,496951,264724,224916,169684]
+    # values = [825594,1677009,1676628,1523970,943972,97426,69666,1296457,1679693,
+    #           1902996,1844992,1049289,1252836,1319836,953277,2067538,675367,853655,
+    #           1826027, 65731,901489,577243,466257,369261]
+    # solution = [1,1,0,1,1,1,0,0,0,1,1,0,1,0,0,1,0,0,0,0,0,1,1,1]
+    # solution = [i for i in range(len(solution)) if solution[i] == 1]
+    # solution = sorted(solution)
+
+    # selected = knapsack(values, weights, capacity)
+    # selected = sorted(selected)
+    # assert selected == solution
 
 
 if __name__ == '__main__':
