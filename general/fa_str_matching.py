9ee3f1e700d02ccc7972fed410874aa10d745502
hongwei <hongwei.ng@adsc.com.sg>
2014-05-22 10:44:31 +0800

    Fixed bug in main.

--- general/fa_str_matching.py
+++ general/fa_str_matching.py
@@ -11,8 +11,15 @@ Algorithms Unlocked by Thomas H. Cormen.
 """
 
 
+import sys
+from collections import defaultdict
+
+
 def build_next_state_table(pattern, charset):
-    pass
+    m = len(pattern)
+    next_state = [defaultdict(int) for _ in range(m + 1)]
+
+    return next_state
 
 
 def match(pattern, text, next_state=None):
@@ -26,9 +33,6 @@ def match(pattern, text, next_state=None):
         state = next_state[state][c]
         if state == m:
             yield i - m
-
-
-from collections import defaultdict
 
 
 def print_matches(pattern, text, matcher):
@@ -46,23 +50,16 @@ def test():
     m = len(pattern)
     next_state = [defaultdict(int) for _ in range(m + 1)]
     next_state[0]['A'] = 1
-
     next_state[1]['A'] = 1
     next_state[1]['C'] = 2
-
     next_state[2]['A'] = 3
-
     next_state[3]['A'] = 1
     next_state[3]['C'] = 4
-
     next_state[4]['A'] = 5
-
     next_state[5]['A'] = 1
     next_state[5]['C'] = 4
     next_state[5]['G'] = 6
-
     next_state[6]['A'] = 7
-
     next_state[7]['A'] = 1
     next_state[7]['C'] = 2
 
@@ -70,14 +67,15 @@ def test():
     print_matches(pattern, text, match(pattern, text, next_state))
 
 
-import sys
 def print_usage():
-    print("{} <pattern> <text>".format(sys.argv[0]))
+    print("Given a pattern and text, find all occurrences of the pattern in the text.")
+    print("Usage: {} <pattern> <text>".format(sys.argv[0]))
 
 
 def main():
-    if len(sys.args) < 3:
+    if len(sys.argv) < 3:
         print_usage()
+        return
 
     pattern, text = sys.argv[1:3]
     print_matches(pattern, text, match(pattern, text))
