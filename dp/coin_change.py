7a964e2b41385ef7be559dae0ef76ea141acd1e2
hongwei <hongwei.ng@adsc.com.sg>
2014-05-25 18:23:27 +0800

    Added final solution to coin change problem.

--- dp/coin_change.py
+++ dp/coin_change.py
@@ -3,23 +3,53 @@
 import numpy as np
 
 
-def build_table(values, C):
+def build_table(denominations, C):
+    """A[i] constains min number of coins needed to change
+    value of i.
+    C is value we want to change for using coins with values
+    in denominations.
+
+    used_coin[i] is actual denomination used for value of i.
+    """
     A = np.zeros(C + 1, dtype=np.int)
-    parents = np.zeros(C+1, dtype=np.int)
+    used_coin = np.zeros(C+1, dtype=np.int)
     for i in range(1, C+1):
-        A[i] = min(1 + A[i- v] for v in values if i >= v)
+        # 1 coin of value denomination + min number needed to change A[i - denomination]
+        possible = [(1 + A[i - denomination],
+                     denomination)
+                    for denomination in denominations
+                    if i >= denomination]
+        A[i], used_coin[i] = min(possible)
 
-    return A
+    return A, used_coin
 
 
-def assemble(values, C, A):
-    pass
+def assemble(C, used_coin):
+    coins = []
+    while C > 0:
+        coins.append(used_coin[C])
+        C -= used_coin[C]
 
+    return coins
+
+
+def print_solution(coins):
+    print("Total number of coins required: {}".format(sum(coins.denominations())))
+    print('-' * 40)
+    for value, count in coins.items():
+        print("{} x {}".format(count, value))
+
+
+from collections import Counter
 def main():
-    values = [1, 2, 5, 10]
-    C = 2
-    A = build_table(values, C)
-    print(A)
+    denominations = [1, 2, 4, 5, 10]
+    C = 29
+    A, used_coin = build_table(denominations, C)
+
+    print("Change: {}".format(C))
+    coins = assemble(C, used_coin)
+    coins = Counter(coins)
+    print_solution(coins)
 
 
 if __name__ == '__main__':
