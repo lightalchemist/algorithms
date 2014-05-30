f2a6c2dcca6505831cd2f9df6711d9d2e13de561
hongwei <lightalchemist@gmail.com>
2014-05-30 22:58:39 +0800

    Refactored code.

--- dp/num_ways_change.py
+++ dp/num_ways_change.py
@@ -6,10 +6,9 @@ Author: Hong-Wei Ng
 Email: lightalchemist@gmail.com
 Github: https://github.com/lightalchemist
 Description: Compute the number of ways to provide change
-for an amount "total" given a set of denominations.
+for an amount "amount" given a set of denominations.
 """
 
-import pdb
 from functools import update_wrapper
 import numpy as np
 
@@ -39,29 +38,31 @@ def memo(f):
 
 
 @memo
-def num_ways_make_change(n, total, D):
+def num_ways_make_change(n, amount, D):
+    """n is number of denominations to consider.
+    D is list of denominations. D[n-1] = nth denomination.
+    """
     if n == 0:  # 0 ways to change any value with 0 coins.
         return 0
 
-    if total == 0:  # 1 way to make 0 change with first n coins
+    if amount == 0:  # 1 way to make 0 change with first n coins
         return 1  # That is use none of the coins.
 
     dn = D[n-1]
-    if dn > total:  # Cannot use nth coin.
-        return num_ways_make_change(n-1, total, D)
+    if dn > amount:  # Cannot use nth coin.
+        return num_ways_make_change(n-1, amount, D)
     else:  # num ways w/o using nth coin + num ways using nth coin.
-        return (num_ways_make_change(n-1, total, D) +
-                num_ways_make_change(n, total-dn, D))
+        return (num_ways_make_change(n-1, amount, D) +
+                num_ways_make_change(n, amount-dn, D))
 
 
-def build_table(total, D):
+def build_table(amount, D):
     nd = len(D)
-    A = np.zeros((nd+1, total+1))
+    A = np.zeros((nd+1, amount+1))
     A[1:, 0] = 1  # 1 way to change for 0 using any number of coins
     for i in range(1, nd+1):  # Use up to coin i
-        for v in range(1, total+1):  # Change v
+        for v in range(1, amount+1):  # Change v
             di = D[i-1]
-            # pdb.set_trace()
             if di > v:
                 A[i][v] = A[i-1][v]
             else:
@@ -72,39 +73,39 @@ def build_table(total, D):
 
 def main():
     denominations = [1, 2]
-    total = 3
-    A = build_table(total, denominations)
-    num_ways = A[len(denominations), total]
+    amount = 3
+    A = build_table(amount, denominations)
+    num_ways = A[len(denominations), amount]
     assert num_ways == 2
     print("Number of ways to change {} using denominations {}:"
-          " {}".format(total, denominations, num_ways))
+          " {}".format(amount, denominations, num_ways))
     num_ways = num_ways_make_change(len(denominations),
-                                    total,
+                                    amount,
                                     denominations)
     print(num_ways)
 
     denominations = [1, 2, 5]
-    total = 10
-    A = build_table(total, denominations)
-    num_ways = A[len(denominations), total]
+    amount = 10
+    A = build_table(amount, denominations)
+    num_ways = A[len(denominations), amount]
     assert num_ways == 10
     print("Number of ways to change {} using denominations {}:"
-          " {}".format(total, denominations, num_ways))
+          " {}".format(amount, denominations, num_ways))
     num_ways = num_ways_make_change(len(denominations),
-                                    total,
+                                    amount,
                                     denominations)
     print(num_ways)
 
     denominations = [1, 5, 10, 25, 50]
-    total = 11
-    A = build_table(total, denominations)
-    num_ways = A[len(denominations), total]
+    amount = 11
+    A = build_table(amount, denominations)
+    num_ways = A[len(denominations), amount]
     assert num_ways == 4
     print("Number of ways to change {} using denominations {}:"
-          " {}".format(total, denominations, num_ways))
-    num_ways = A[len(denominations), total]
+          " {}".format(amount, denominations, num_ways))
+    num_ways = A[len(denominations), amount]
     num_ways = num_ways_make_change(len(denominations),
-                                    total,
+                                    amount,
                                     denominations)
     print(num_ways)
 
