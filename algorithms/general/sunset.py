19cf1956b48d4366e41e0eea0a0bead5f6946526
hongwei <lightalchemist@gmail.com>
2014-06-12 15:21:52 +0800

    Added more description.

--- algorithms/general/sunset.py
+++ algorithms/general/sunset.py
@@ -7,6 +7,18 @@ Author: Hong-Wei Ng
 Email: lightalchemist@gmail.com
 Github: https://github.com/lightalchemist
 Description: Solution to problem 8.6 pg 69 of Elements of Programming Interview book.
+
+A number of buildings are lined up along a East-West line.
+A particular building b has a view of the sunset if its height is greater than
+that of all buildings to its west.
+
+Given the heights of buildings ordered from east to west,
+find the buildings that has a view of the sunset.
+
+Here we represent the building by its height.
+Note that these heights has to be unique as otherwise
+one building will be blocking the view of another since
+they have the same height.
 """
 
 from stack import Stack
