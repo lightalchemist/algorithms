130729380fa3aee1e544e1f1f95cd58b604734ba
hongwei <lightalchemist@gmail.com>
2014-07-01 00:03:31 +0800

    Implemented selection sort

---
+++ algorithms/general/selection_sort.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+
+def sort(seq):
+    minIdx = 0
+
+    N = len(seq)
+    for i in range(N):
+        min_item = float('inf')
+        min_idx = i
+        # Find min item from i to end and swap into pos i
+        for j in range(i, N):
+            if seq[j] < min_item:
+                min_item = seq[j]
+                min_idx = j
+
+        seq[i], seq[min_idx] = seq[min_idx], seq[i]
+
+
+import copy
+import random
+
+
+def test():
+    a = range(10)
+    random.shuffle(a)
+    b = copy.copy(a)
+    sort(a)
+    assert a == sorted(b)
+
+    a = [1, 1, 1, 1]
+    sort(a)
+    assert a == [1, 1, 1, 1]
+
+    a = [-1]
+    sort(a)
+    assert a == [-1]
+
+    a = [random.randint(-100, 100) for _ in range(100)]
+    b = copy.copy(a)
+    sort(a)
+    assert a == sorted(b)
+
+
+if __name__ == '__test__':
+    test()
