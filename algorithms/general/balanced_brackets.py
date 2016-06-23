89c220a0799862f7894f4819b9d34a6b676be996
hongwei <lightalchemist@gmail.com>
2016-06-23 22:31:29 +0800

    Added another implementation of balanced_brackets

--- algorithms/general/balanced_brackets.py
+++ algorithms/general/balanced_brackets.py
@@ -37,32 +37,60 @@ def verify_balanced_brackets(expr):
     return len(stack) == 0
 
 
+def verify_balanced_brackets2(expr):
+    # This version can't handle ' and " because
+    # it can't tell when it is meant to open and when to close
+    # In principle it should be faster than verify_balanced_brackets2
+    # as it allows for early termination
+
+    pair = {')': '(',
+            ']': '[',
+            # "'": "'",
+            # '"': '"',
+            '}': '{'}
+
+    opening_brackets = set(pair.values())
+    closing_brackets = set(pair.keys())
+    stack = []
+    for c in expr:
+        if c in opening_brackets:
+            stack.append(c)
+        elif c in closing_brackets:
+            if not stack:
+                return False
+            top = stack.pop()
+            if pair.get(c, None) != top:
+                return False
+
+    return True
+
+
 def main():
     expr = "([])[]()"
-    assert(verify_balanced_brackets(expr))
+    assert(verify_balanced_brackets2(expr))
 
     expr = "((([()]))())"
-    assert(verify_balanced_brackets(expr))
+    assert(verify_balanced_brackets2(expr))
 
     expr = "{([<>])}"
-    assert(verify_balanced_brackets(expr))
+    assert(verify_balanced_brackets2(expr))
 
     code_snippet = """if (value == 10) {
                           print("Value is 10");
                       }
                    """
-    assert(verify_balanced_brackets(code_snippet))
+    assert(verify_balanced_brackets2(code_snippet))
 
-    assert(verify_balanced_brackets(""))
+    assert(verify_balanced_brackets2(""))
 
     expr = "]"
-    assert(verify_balanced_brackets(expr) == False)
+    assert(verify_balanced_brackets2(expr) == False)
 
     expr = "]["
-    assert(verify_balanced_brackets(expr) == False)
+    assert(verify_balanced_brackets2(expr) == False)
 
     expr = "([]]()"
-    assert(verify_balanced_brackets(expr) == False)
+    assert(verify_balanced_brackets2(expr) == False)
 
 
 
